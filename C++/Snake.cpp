d966ce10fa69e357203ea5b30a71b83087c330d0
Софья Михайлова <91487865+Sophya133@users.noreply.github.com>
2024-07-24 15:33:04 +0300

    1
    добавлена модель кольца

--- C++/Snake.cpp
+++ C++/Snake.cpp
@@ -145,3 +145,20 @@ int main() {
     cout << "Game over! Time's up!" << endl;
     return 0;
 }
+
+
+left{
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
